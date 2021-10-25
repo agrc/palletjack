79fcd5aeb6441b235540592b7e47135a1af8fb7e
Jake Adams <jdadams@utah.gov>
2021-10-25 17:42:38 -0600

    feat: ColorRampReclassifier

--- tests/test_data_coupler.py
+++ tests/test_data_coupler.py
@@ -1,7 +1,9 @@
+import json
 from pathlib import Path
 
 import numpy as np
 import pandas as pd
+import pytest
 
 from era import data_coupler
 
@@ -79,3 +81,138 @@ class TestSFTPLoader:
         data_coupler.SFTPLoader.read_csv_into_dataframe(sftploader_mock, 'baz')
 
         pd_mock.assert_called_with(Path('foo', 'baz'), names=None, dtype=None)
+
+
+class TestColorRampReclassifier:
+
+    def test_get_layer_id_returns_match_single_layer(self, mocker):
+        layers = {
+            'operationalLayers': [
+                {
+                    'title': 'foo'
+                },
+            ],
+        }
+        get_data_mock = mocker.Mock(return_value=layers)
+        webmap_item_mock = mocker.Mock()
+        webmap_item_mock.get_data = get_data_mock
+        reclassifier = data_coupler.ColorRampReclassifier(webmap_item_mock, 'gis')
+
+        layer_id = reclassifier._get_layer_id('foo')
+        assert layer_id == 0
+
+    def test_get_layer_id_returns_match_many_layers(self, mocker):
+        layers = {
+            'operationalLayers': [
+                {
+                    'title': 'foo'
+                },
+                {
+                    'title': 'bar'
+                },
+                {
+                    'title': 'baz'
+                },
+            ],
+        }
+        get_data_mock = mocker.Mock(return_value=layers)
+        webmap_item_mock = mocker.Mock()
+        webmap_item_mock.get_data = get_data_mock
+        reclassifier = data_coupler.ColorRampReclassifier(webmap_item_mock, 'gis')
+
+        layer_id = reclassifier._get_layer_id('bar')
+        assert layer_id == 1
+
+    def test_get_layer_id_returns_first_match(self, mocker):
+        layers = {
+            'operationalLayers': [
+                {
+                    'title': 'foo'
+                },
+                {
+                    'title': 'bar'
+                },
+                {
+                    'title': 'bar'
+                },
+            ],
+        }
+        get_data_mock = mocker.Mock(return_value=layers)
+        webmap_item_mock = mocker.Mock()
+        webmap_item_mock.get_data = get_data_mock
+        reclassifier = data_coupler.ColorRampReclassifier(webmap_item_mock, 'gis')
+
+        layer_id = reclassifier._get_layer_id('bar')
+        assert layer_id == 1
+
+    def test_get_layer_id_raises_error_when_not_found(self, mocker):
+        layers = {
+            'operationalLayers': [
+                {
+                    'title': 'bar'
+                },
+            ],
+        }
+        get_data_mock = mocker.Mock(return_value=layers)
+        webmap_item_mock = mocker.Mock()
+        webmap_item_mock.title = 'test map'
+        webmap_item_mock.get_data = get_data_mock
+        reclassifier = data_coupler.ColorRampReclassifier(webmap_item_mock, 'gis')
+
+        with pytest.raises(ValueError) as error_info:
+            layer_id = reclassifier._get_layer_id('foo')
+
+        assert 'Could not find "foo" in test map' in str(error_info.value)
+
+    def test_calculate_new_stops_with_manual_numbers(self):
+        dataframe = pd.DataFrame({'numbers': [100, 300, 500, 700, 900]})
+
+        stops = data_coupler.ColorRampReclassifier._calculate_new_stops(dataframe, 'numbers', 5)
+
+        assert stops == [100, 279, 458, 637, 816]
+
+    def test_update_stops_values(self, mocker):
+        # renderer = data['operationalLayers'][layer_number]['layerDefinition']['drawingInfo']['renderer']
+        # stops = renderer['visualVariables'][0]['stops']
+
+        data = {
+            'operationalLayers': [{
+                'layerDefinition': {
+                    'drawingInfo': {
+                        'renderer': {
+                            'visualVariables': [{
+                                'stops': [{
+                                    'value': 0
+                                }, {
+                                    'value': 1
+                                }, {
+                                    'value': 2
+                                }, {
+                                    'value': 3
+                                }]
+                            }]
+                        }
+                    }
+                }
+            }],
+        }
+        get_data_mock = mocker.Mock(return_value=data)
+        webmap_item_mock = mocker.Mock()
+        webmap_item_mock.get_data = get_data_mock
+        update_mock = mocker.Mock()
+        webmap_item_mock.update = update_mock
+        reclassifier = data_coupler.ColorRampReclassifier(webmap_item_mock, 'gis')
+
+        reclassifier._update_stop_values(0, [100, 200, 300, 400])
+
+        data['operationalLayers'][0]['layerDefinition']['drawingInfo']['renderer']['visualVariables'][0]['stops'] = [{
+            'value': 100
+        }, {
+            'value': 200
+        }, {
+            'value': 300
+        }, {
+            'value': 400
+        }]
+
+        assert update_mock.called_with(item_properties={'text': json.dumps(data)})
