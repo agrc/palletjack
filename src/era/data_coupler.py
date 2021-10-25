8b7b70671d46c80ea209813d3ad5f6322b7fc1a0
Jake Adams <jdadams@utah.gov>
2021-10-25 12:03:41 -0600

    feat: more tests and fixes

--- src/era/data_coupler.py
+++ src/era/data_coupler.py
@@ -1,9 +1,11 @@
 """Objects for bringing data from SFTP-hosted CSVs into AGOL Feature Services
 """
 
+import json
 from pathlib import Path
 
 import arcpy
+import numpy as np
 import pandas as pd
 import pysftp
 
@@ -46,7 +48,7 @@ class SFTPLoader:
         filepath = Path(self.download_dir, filename)
         column_names = None
         if column_types:
-            column_names = column_types.keys()
+            column_names = list(column_types.keys())
         dataframe = pd.read_csv(filepath, names=column_names, dtype=column_types)
         return dataframe
 
@@ -55,14 +57,18 @@ class FeatureServiceInLineUpdater:
     """Updates an AGOL Feature Service with data from a pandas DataFrame
     """
 
-    def __init__(self, dataframe):
+    def __init__(self, dataframe, index_column):
         self.dataframe = dataframe
-        self.data_as_dict = {}
-
-    def dataframe_to_dict(self, index_column):
         self.data_as_dict = self.dataframe.set_index(index_column).to_dict('index')
 
     def update_feature_service(self, feature_service_url, fields):
+        """Update a feature service in-place with data from pandas data frame using UpdateCursor
+
+        Args:
+            feature_service_url (str): URL to feature service
+            fields (list): Names of fields to update
+        """
+
         with arcpy.da.UpdateCursor(feature_service_url, fields) as update_cursor:
             for row in update_cursor:
                 key = row[0]
