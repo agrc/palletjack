04678d8289e3a56655432a44449965e16947d873
Jake Adams <jdadams@utah.gov>
2021-10-25 12:03:41 -0600

    feat: more tests and fixes

--- tests/test_data_coupler.py
+++ tests/test_data_coupler.py
@@ -1,3 +1,8 @@
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+
 from era import data_coupler
 
 
@@ -24,3 +29,53 @@ class TestFeatureServiceInLineUpdater:
 
         contextmanager_mock.updateRow.assert_called_with(['12345', '57', 100.00, '1/1/2022'])
         contextmanager_mock.updateRow.assert_called_once()
+
+
+class TestSFTPLoader:
+
+    def test_download_sftp_files_uses_right_credentials(self, mocker):
+        sftploader_mock = mocker.Mock()
+        sftploader_mock.secrets.KNOWNHOSTS = 'knownhosts_file'
+        sftploader_mock.secrets.SFTP_HOST = 'sftp_host'
+        sftploader_mock.secrets.SFTP_USERNAME = 'username'
+        sftploader_mock.secrets.SFTP_PASSWORD = 'password'
+        sftploader_mock.download_dir = 'download_dir'
+
+        connection_mock = mocker.MagicMock()
+        context_manager_mock = mocker.MagicMock()
+        context_manager_mock.return_value.__enter__.return_value = connection_mock
+        mocker.patch('pysftp.Connection', new=context_manager_mock)
+
+        cnopts_mock = mocker.Mock()
+        cnopts_mock.side_effect = lambda knownhosts: knownhosts
+        mocker.patch('pysftp.CnOpts', new=cnopts_mock)
+
+        data_coupler.SFTPLoader.download_sftp_files(sftploader_mock)
+
+        context_manager_mock.assert_called_with(
+            'sftp_host', username='username', password='password', cnopts='knownhosts_file'
+        )
+
+    def test_read_csv_into_dataframe_with_column_names(self, mocker):
+        pd_mock = mocker.Mock()
+        mocker.patch.object(pd, 'read_csv', new=pd_mock)
+
+        sftploader_mock = mocker.Mock()
+        sftploader_mock.download_dir = 'foo'
+
+        column_types = {'bar': np.float64}
+
+        data_coupler.SFTPLoader.read_csv_into_dataframe(sftploader_mock, 'baz', column_types)
+
+        pd_mock.assert_called_with(Path('foo', 'baz'), names=['bar'], dtype=column_types)
+
+    def test_read_csv_into_dataframe_no_column_names(self, mocker):
+        pd_mock = mocker.Mock()
+        mocker.patch.object(pd, 'read_csv', new=pd_mock)
+
+        sftploader_mock = mocker.Mock()
+        sftploader_mock.download_dir = 'foo'
+
+        data_coupler.SFTPLoader.read_csv_into_dataframe(sftploader_mock, 'baz')
+
+        pd_mock.assert_called_with(Path('foo', 'baz'), names=None, dtype=None)
