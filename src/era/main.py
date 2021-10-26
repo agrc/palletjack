f9002e637c27b1c4bb4303738ddf8be64367d5fb
Jake Adams <jdadams@utah.gov>
2021-10-25 18:03:06 -0600

    wip: rough, untested main script

--- src/era/main.py
+++ src/era/main.py
@@ -1,19 +1,51 @@
 #!/usr/bin/env python
 # * coding: utf8 *
 """
-a description of what this module does.
-this file is for testing linting...
+Updates the DWS ERAP layer based on their weekly
 """
 
+from pathlib import Path
+
+import arcgis
+import numpy as np
 from supervisor.message_handlers import SendGridHandler
 from supervisor.models import MessageDetails, Supervisor
 
-from . import secrets, updates
+from . import secrets
+from .data_coupler import ColorRampReclassifier, FeatureServiceInLineUpdater, SFTPLoader
 
-era_supervisor = Supervisor()
 
-era_supervisor.add_message_handler(SendGridHandler(secrets.SENDGRID_SETTINGS, 'ERA'))
+def process():
+    erap_supervisor = Supervisor()
+
+    erap_supervisor.add_message_handler(SendGridHandler(secrets.SENDGRID_SETTINGS, 'ERA'))
+
+    erap_download_dir = Path()
+    erap_data_types = {
+        'ZipCode': str,
+        'Count': str,
+        'Amount': np.float64,
+        'LastUpdated': str,
+    }
+    erap_feature_service_url = ''
+    gis = arcgis.gis.GIS(secrets.AGOL_ORG, secrets.AGOL_USER, secrets.AGOL_PASSWORD)
+    erap_webmap_item = gis.content.get('')
+    erap_layer_name = ''
+
+    #: Load the latest data from FTP
+    erap_loader = SFTPLoader(secrets, erap_download_dir)
+    erap_loader.download_sftp_files()
+    dataframe = erap_loader.read_csv_into_dataframe('ERAP_PAYMENTS.csv', erap_data_types)
+
+    #: Update the AGOL data
+    erap_updater = FeatureServiceInLineUpdater(dataframe, 'ZipCode')
+    erap_updater.update_feature_service(erap_feature_service_url, ['Count', 'Amount', 'LastUpdated'])
+
+    #: Reclassify the break values on the webmap's color ramp
+    erap_reclassifier = ColorRampReclassifier(erap_webmap_item, gis)
+    success = erap_reclassifier.update_color_ramp_values(erap_layer_name, 'Amount')
+
 
 if __name__ == '__main__':
     #: the code that executes if you run the file or module directly
-    pass
+    process()
