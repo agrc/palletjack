8b163319df39e59e14c77f4082956f453bf231d5
Jake Adams <jdadams@utah.gov>
2021-10-28 10:35:50 -0600

    feat: new test for column check

--- tests/test_data_coupler.py
+++ tests/test_data_coupler.py
@@ -13,14 +13,14 @@ class TestFeatureServiceInLineUpdater:
     def test_update_feature_service(self, mocker):
         #: We create a mock that will be returned by UpdateCursor's mock's __enter__, thus becoming our context manager.
         #: We then set it's __iter__.return_value to define the data we want it to iterate over.
-        contextmanager_mock = mocker.MagicMock()
-        contextmanager_mock.__iter__.return_value = [
+        cursor_mock = mocker.MagicMock()
+        cursor_mock.__iter__.return_value = [
             ['12345', '42', 123.45, '12/25/2021'],
             ['67890', '18', 67.89, '12/25/2021'],
         ]
-        da_mock = mocker.MagicMock()
-        da_mock.return_value.__enter__.return_value = contextmanager_mock
-        mocker.patch('arcpy.da.UpdateCursor', new=da_mock)
+        context_manager_mock = mocker.MagicMock()
+        context_manager_mock.return_value.__enter__.return_value = cursor_mock
+        mocker.patch('arcpy.da.UpdateCursor', new=context_manager_mock)
 
         fsupdater_mock = mocker.Mock()
         fsupdater_mock.data_as_dict = {'12345': {'Count': '57', 'Amount': 100.00, 'Date': '1/1/2022'}}
@@ -29,8 +29,8 @@ class TestFeatureServiceInLineUpdater:
             fsupdater_mock, 'foo', ['ZipCode', 'Count', 'Amount', 'Date']
         )
 
-        contextmanager_mock.updateRow.assert_called_with(['12345', '57', 100.00, '1/1/2022'])
-        contextmanager_mock.updateRow.assert_called_once()
+        cursor_mock.updateRow.assert_called_with(['12345', '57', 100.00, '1/1/2022'])
+        cursor_mock.updateRow.assert_called_once()
 
 
 class TestSFTPLoader:
@@ -41,7 +41,9 @@ class TestSFTPLoader:
         sftploader_mock.secrets.SFTP_HOST = 'sftp_host'
         sftploader_mock.secrets.SFTP_USERNAME = 'username'
         sftploader_mock.secrets.SFTP_PASSWORD = 'password'
-        sftploader_mock.download_dir = 'download_dir'
+        download_dir_mock = mocker.Mock()
+        download_dir_mock.iterdir.side_effect = [[], ['file_a', 'file_b']]
+        sftploader_mock.download_dir = download_dir_mock
 
         connection_mock = mocker.MagicMock()
         context_manager_mock = mocker.MagicMock()
@@ -172,6 +174,13 @@ class TestColorRampReclassifier:
         stops = data_coupler.ColorRampReclassifier._calculate_new_stops(dataframe, 'numbers', 5)
 
         assert stops == [100, 279, 458, 637, 816]
+
+    def test_calculate_new_stops_mistmached_column_raises_error(self):
+        dataframe = pd.DataFrame({'numbers': [100, 300, 500, 700, 900]})
+
+        with pytest.raises(ValueError) as error_info:
+            stops = data_coupler.ColorRampReclassifier._calculate_new_stops(dataframe, 'foo', 5)
+            assert 'Column `foo` not in dataframe`' in str(error_info)
 
     def test_update_stops_values(self, mocker):
         # renderer = data['operationalLayers'][layer_number]['layerDefinition']['drawingInfo']['renderer']
