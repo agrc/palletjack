216729e4bc087adf58c60566662c89dd627abf76
Jake Adams <jdadams@utah.gov>
2022-05-19 08:35:23 -0600

    chore: test/example drive/sheet usage code

---
+++ src/palletjack/examples.py
@@ -0,0 +1,19 @@
+from palletjack import GoogleDriveDownloader, GSheetLoader
+
+sheet_id = ''
+
+gsheetloader = GSheetLoader(r'c:\gis\git\pj-uorg\src\uorg\sheets-sa.json')
+worksheets = gsheetloader.load_all_worksheets_into_dataframes(sheet_id)
+
+uorg_2021 = worksheets['2021']
+pics_list = list(uorg_2021['Picture'])
+
+downloader = GoogleDriveDownloader(r'c:\temp\google_python_tests')
+for pic in pics_list:
+    if not pic:
+        print('No link in cell')
+        continue
+    try:
+        downloader.download_file_from_google_drive(pic)
+    except RuntimeError as err:
+        print(err)
