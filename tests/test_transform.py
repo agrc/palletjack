650386f3718f5ecf8526e02973ae4bbdcd8178bb
Jake Adams <jdadams@utah.gov>
2022-07-11 16:24:46 -0600

    chore: pass kwargs through to geocoder

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -29,7 +29,36 @@ class TestAPIGeocoder:
 
         geocoder.geocode_dataframe(test_df, 'street', 'zip', 3857)
 
-        palletjack.transform.utils.geocode_addr.call_args.args == [
-            pd.array(['4501 S Constitution Blvd', '84129']), 'street', 'zip', 'foo'
+        assert list(palletjack.transform.utils.geocode_addr.call_args.args[0]) == ['4501 S Constitution Blvd', '84129']
+        assert palletjack.transform.utils.geocode_addr.call_args.args[1:] == ('street', 'zip', 'foo')
+        assert palletjack.transform.utils.geocode_addr.call_args.kwargs == {'spatialReference': '3857'}
+
+    def test_geocode_dataframe_passes_kwargs_through_to_util_method(self, mocker):
+        mocker.patch.object(palletjack.transform, 'utils', autospec=True)
+        palletjack.transform.utils.geocode_addr.side_effect = [
+            {
+                'x': 123,
+                'y': 456
+            },
+            {
+                'x': 789,
+                'y': 101
+            },
         ]
-        palletjack.transform.utils.geocode_addr.call_args.kwargs == {'spatialReference': '3857'}
+        mocker.patch.object(pd.DataFrame.spatial, 'from_xy')
+
+        geocoder = palletjack.transform.APIGeocoder('foo')
+
+        test_df = pd.DataFrame({
+            'street': ['4315 S 2700 W', '4501 S Constitution Blvd'],
+            'zip': ['84129', '84129'],
+        })
+
+        geocoder.geocode_dataframe(test_df, 'street', 'zip', 3857, acceptScore=80)
+
+        assert list(palletjack.transform.utils.geocode_addr.call_args.args[0]) == ['4501 S Constitution Blvd', '84129']
+        assert palletjack.transform.utils.geocode_addr.call_args.args[1:] == ('street', 'zip', 'foo')
+        assert palletjack.transform.utils.geocode_addr.call_args.kwargs == {
+            'spatialReference': '3857',
+            'acceptScore': 80
+        }
