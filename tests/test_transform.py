c0b1bf887b419bccc57306ed780064fb9a2fb6ad
Jake Adams <jdadams@utah.gov>
2022-07-13 18:18:38 -0600

    chore: use itertuples instead of apply

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -26,12 +26,10 @@ class TestAPIGeocoder:
 
         geocoder.geocode_dataframe(test_df, 'street', 'zip', 3857)
 
-        assert list(palletjack.transform.utils.geocode_addr.call_args.args[0]) == ['4501 S Constitution Blvd', '84129']
-        assert palletjack.transform.utils.geocode_addr.call_args.args[1:] == ('street', 'zip', 'foo')
-        assert palletjack.transform.utils.geocode_addr.call_args.kwargs == {
-            'rate_limits': (0.015, 0.03),
-            'spatialReference': '3857'
-        }
+        assert palletjack.transform.utils.geocode_addr.call_args.args == (
+            '4501 S Constitution Blvd', '84129', 'foo', (0.015, 0.03)
+        )
+        assert palletjack.transform.utils.geocode_addr.call_args.kwargs == {'spatialReference': '3857'}
 
     def test_geocode_dataframe_passes_kwargs_through_to_util_method(self, mocker):
         mocker.patch.object(palletjack.transform, 'utils', autospec=True)
@@ -50,13 +48,77 @@ class TestAPIGeocoder:
 
         geocoder.geocode_dataframe(test_df, 'street', 'zip', 3857, acceptScore=80)
 
-        assert list(palletjack.transform.utils.geocode_addr.call_args.args[0]) == ['4501 S Constitution Blvd', '84129']
-        assert palletjack.transform.utils.geocode_addr.call_args.args[1:] == ('street', 'zip', 'foo')
+        assert palletjack.transform.utils.geocode_addr.call_args.args == (
+            '4501 S Constitution Blvd', '84129', 'foo', (0.015, 0.03)
+        )
         assert palletjack.transform.utils.geocode_addr.call_args.kwargs == {
-            'rate_limits': (0.015, 0.03),
             'spatialReference': '3857',
             'acceptScore': 80
         }
+
+    def test_geocode_dataframe_builds_output_dataframe(self, mocker, caplog):
+        caplog.set_level(logging.DEBUG)
+        mocker.patch('palletjack.utils.requests', autospec=True)
+        mocker.patch('palletjack.utils.sleep')
+
+        first_response = mocker.Mock()
+        first_response.json.return_value = {
+            'status': 200,
+            'result': {
+                'location': {
+                    'x': 123,
+                    'y': 456
+                },
+                'score': 100.,
+                'matchAddress': 'bar'
+            }
+        }
+        first_response.status_code = 200
+
+        second_response = mocker.Mock()
+        second_response.json.return_value = {
+            'status': 200,
+            'result': {
+                'location': {
+                    'x': 789,
+                    'y': 101
+                },
+                'score': 100.,
+                'matchAddress': 'baz'
+            }
+        }
+        second_response.status_code = 200
+
+        palletjack.utils.requests.get.side_effect = [first_response, second_response]
+
+        #: Get patch from_xy to just return the dataframe it was passed so we don't have to create a spatial one for
+        #: testing
+        mocker.patch.object(pd.DataFrame.spatial, 'from_xy')
+
+        def _mock_from_xy(dataframe, x, y, sr=None):
+            return dataframe
+
+        pd.DataFrame.spatial.from_xy.side_effect = _mock_from_xy
+
+        geocoder = palletjack.transform.APIGeocoder('foo')
+
+        input_df = pd.DataFrame({
+            'street': ['4315 S 2700 W', '4501 S Constitution Blvd'],
+            'zip': ['84129', '84129'],
+        })
+
+        geocoded_df = geocoder.geocode_dataframe(input_df, 'street', 'zip', 26912)
+
+        test_df = pd.DataFrame({
+            'street': ['4315 S 2700 W', '4501 S Constitution Blvd'],
+            'zip': ['84129', '84129'],
+            'x': [123, 789],
+            'y': [456, 101],
+            'score': [100., 100.],
+            'matchAddress': ['bar', 'baz']
+        })
+
+        tm.assert_frame_equal(test_df, geocoded_df)
 
     def test_geocode_dataframe_continues_after_failed_row(self, mocker, caplog):
         caplog.set_level(logging.DEBUG)
@@ -108,10 +170,10 @@ class TestAPIGeocoder:
             'x': [0, 123],
             'y': [0, 456],
             'score': [0., 100.],
-            'match_addr': ['No API response', 'bar']
+            'matchAddress': ['No API response', 'bar']
         })
 
         tm.assert_frame_equal(test_df, geocoded_df)
-        assert 'ERROR    palletjack.utils:utils.py:200 Did not receive a valid geocoding response; status code: 500' in caplog.text
+        assert 'ERROR    palletjack.utils:utils.py:199 Did not receive a valid geocoding response; status code: 500' in caplog.text
         assert palletjack.utils.requests.get.call_count == 5
         assert palletjack.utils.sleep.call_count == 5
