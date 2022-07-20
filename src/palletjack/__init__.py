f4c96d8f1b9614fa839088632bcbac2d7834967c
Jake Adams <jdadams@utah.gov>
2022-07-19 19:05:39 -0600

    chore: rename updaters to load

--- src/palletjack/__init__.py
+++ src/palletjack/__init__.py
@@ -1,8 +1,4 @@
 """A library for updating AGOL feature services with data from SFTP sources
 """
 
-from . import extract, transform
-# from .extract import GoogleDriveDownloader, GSheetLoader, SFTPLoader
-from .updaters import (
-    ColorRampReclassifier, FeatureServiceAttachmentsUpdater, FeatureServiceInlineUpdater, FeatureServiceOverwriter
-)
+from . import extract, load, transform
