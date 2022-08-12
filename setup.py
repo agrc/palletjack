8a2900979c0d2f3f06d4845e12850dad48cb1b97
Jake Adams <jdadams@utah.gov>
2022-08-12 13:25:04 -0600

    docs: upserter, overwritter api docs

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='ugrc-palletjack',
-    version='2.5.0',
+    version='2.5.1',
     license='MIT',
     description='Updating AGOL feature services with data from SFTP shares.',
     author='Jake Adams, UGRC',
