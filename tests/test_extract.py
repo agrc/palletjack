d65d6509b281d4930aded3648426434ba01018be
Jake Adams <jdadams@utah.gov>
2023-01-04 11:05:11 -0700

    feat: scotts postgres loader

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -1,6 +1,8 @@
 import logging
 from pathlib import Path
 
+import arcgis
+import geopandas as gpd
 import numpy as np
 import pandas as pd
 import pytest
@@ -587,3 +589,17 @@ class TestSFTPLoader:
         extract.SFTPLoader.read_csv_into_dataframe(sftploader_mock, 'baz')
 
         pd_mock.assert_called_with(Path('foo', 'baz'), names=None, dtype=None)
+
+
+class TestPostgresLoader:
+
+    def test_get_postgres_connection(self, mocker):
+
+        mocker.patch.object(
+            gpd, 'read_postgis', return_value=gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+        )
+
+        loader = extract.PostgresLoader('host', 'app', 'user', 'password')
+        dataframe = loader.read_table_into_dataframe('table', 'name', '4326', 'geometry')
+
+        assert dataframe.spatial.geometry_type == ['polygon']
