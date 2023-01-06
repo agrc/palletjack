636fb3722b6d26bfd782a47605c5b020bdaabd52
Jake Adams <jdadams@utah.gov>
2023-01-06 09:06:16 -0700

    refactor: import utils in init for quality of life

--- src/palletjack/__init__.py
+++ src/palletjack/__init__.py
@@ -1,4 +1,4 @@
 """A library for updating AGOL feature services automatically with data from external sources.
 """
 
-from . import extract, load, transform
+from . import extract, load, transform, utils
