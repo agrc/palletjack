6a389a3e1fd9732bdbe5e9ab4de99cd99232d4f6
Jake Adams <jdadams@utah.gov>
2023-01-26 10:09:58 -0700

    fix: raise on timezone-aware datetimes

--- src/palletjack/errors.py
+++ src/palletjack/errors.py
@@ -1,2 +1,6 @@
 class IntFieldAsFloatError(Exception):
     """A field expected to be integer is float instead"""
+
+
+class TimezoneAwareDatetimeError(Exception):
+    """A datetime field uses a timezone-aware dtype"""
