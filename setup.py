9b7c602a6d3b08f5ed942e2d89d125a744c87099
Jake Adams <jdadams@utah.gov>
2023-01-26 18:06:44 -0700

    chore: beta version number

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='ugrc-palletjack',
-    version='3.0.0',
+    version='3.0.0.beta1',
     license='MIT',
     description='Updating AGOL feature services with data from SFTP shares.',
     author='Jake Adams, UGRC',
