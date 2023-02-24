45ac12bf544d939a17e182dd51c0bbb2bd86fe98
Jake Adams <jdadams@utah.gov>
2023-02-24 13:13:01 -0700

    chore: updated setup.py, numpy version

--- setup.py
+++ setup.py
@@ -4,28 +4,28 @@
 setup.py
 A module that installs palletjack as a module
 """
-from glob import glob
-from os.path import basename, splitext
+from pathlib import Path
 
 from setuptools import find_packages, setup
 
 setup(
     name='ugrc-palletjack',
-    version='3.0.0.beta1',
-    license='MIT',
+    version='3.0.0.beta2',
     description='Updating AGOL feature services with data from SFTP shares.',
+    long_description=(Path(__file__).parent / 'README.md').read_text(),
+    long_description_content_type='text/markdown',
     author='Jake Adams, UGRC',
     author_email='jdadams@utah.gov',
     url='https://github.com/agrc/palletjack',
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
     include_package_data=True,
     zip_safe=True,
     classifiers=[
         # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
         'Topic :: Utilities',
     ],
     project_urls={
@@ -40,7 +40,7 @@ setup(
         'SQLAlchemy==1.4.*',
         'pg8000==1.29.*',
         'psycopg2-binary==2.9.*',
-        'numpy==1.24.*',  #: May need to pin to 1.23.* to fix "module 'numpy' has no attribute 'str'" error
+        'numpy==1.23.*',  #: Pinned to 1.23.* to fix "module 'numpy' has no attribute 'str'" error
     ],
     extras_require={
         'tests': [
