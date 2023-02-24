f198467fec9391653d25d77d7b9aa6f311b44fce
Jake Adams <jdadams@utah.gov>
2023-02-24 13:37:38 -0700

    ci: publish via action

---
+++ src/palletjack/version.py
@@ -0,0 +1,6 @@
+"""A single source of truth for the version in a programatically-accessible variable.
+This must only include a single line: __version__ = '0.0.0'
+"""
+
+#: This will be automatically set by the publish action when it is uploaded to pypi; only change this for local dev
+__version__ = '0.0.0'
