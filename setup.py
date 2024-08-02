7e5c8d322aedaa0dd1d8b06ae677b0946a2b56c0
stdavis <stdavis@utah.gov>
2024-08-02 11:32:47 -0600

    fix: run ruff as separate command
    pytest-ruff was preventing tests from running in VSCode or at all in my command line on mac.

--- setup.py
+++ setup.py
@@ -52,12 +52,11 @@ setup(
             "pytest-cov>=3,<6",
             "pytest-instafail~=0.4",
             "pytest-mock>=3.10,<3.15",
-            "pytest-ruff==0.*",
             "pytest-watch~=4.2",
             "pytest>=6,<9",
             "black>=23.3,<24.5",
             "requests-mock==1.*",
-            "ruff==0.0.*",
+            "ruff==0.*",
         ]
     },
     setup_requires=[
