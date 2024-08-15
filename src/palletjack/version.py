f5d96472076bb21f32542bc56ad2a2f5e9859dc6
stdavis <stdavis@utah.gov>
2024-08-15 16:45:50 -0600

    fix: remove version number in comments that is confusion release-please

--- src/palletjack/version.py
+++ src/palletjack/version.py
@@ -1,5 +1,5 @@
 """A single source of truth for the version in a programmatically-accessible variable.
-This must only include a single line: __version__ = '0.0.0'
+This file must only include the single line of code below
 """
 
 #: This will be automatically set by the publish action when it is uploaded to pypi; only change this for local dev
