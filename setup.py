9024368296706b07be957ea58582be43d985b9ae
Jake Adams <jdadams@utah.gov>
2025-07-31 15:21:54 -0600

    deps: update for dbot

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     keywords=["gis"],
     install_requires=[
         "arcgis>=2.3,<2.5",
-        "geopandas>=0.14,<1.1",
+        "geopandas>=0.14,<1.2",
         "geodatasets>=2023.12,<2024.9",
         "pg8000>=1.29,<1.32",
         "psycopg2-binary==2.9.*",
