90ad35d61c9c3efc6be30edd9eab88db160a1f61
Jake Adams <jdadams@utah.gov>
2025-12-15 16:42:40 -0700

    ci: try forcing locale

--- src/palletjack/__init__.py
+++ src/palletjack/__init__.py
@@ -9,5 +9,4 @@ from . import extract, load, transform, utils  # noqa: F401
 from .errors import IntFieldAsFloatError, TimezoneAwareDatetimeError  # noqa: F401
 
 #: If the locale is not set explicitly, set it to the system default for text to number conversions
-if not locale.getlocale(locale.LC_NUMERIC)[0]:
-    locale.setlocale(locale.LC_NUMERIC, locale.getlocale())
+locale.setlocale(locale.LC_NUMERIC, ("en_US", "UTF-8"))
