6c00e48b2410ec216356684e298ec15a132da912
Jake Adams <jdadams@utah.gov>
2025-12-15 16:49:55 -0700

    ci: keep locale if set explicitly

--- src/palletjack/__init__.py
+++ src/palletjack/__init__.py
@@ -8,5 +8,7 @@ import locale
 from . import extract, load, transform, utils  # noqa: F401
 from .errors import IntFieldAsFloatError, TimezoneAwareDatetimeError  # noqa: F401
 
-#: If the locale is not set explicitly, set it to the system default for text to number conversions
-locale.setlocale(locale.LC_NUMERIC, ("en_US", "UTF-8"))
+#: If the locale number category is not set explicitly, set it to en_US and UTF-8 encoding to facilitate parsing
+#: strings with comma separators to numeric types
+if not locale.getlocale(locale.LC_NUMERIC)[0]:
+    locale.setlocale(locale.LC_NUMERIC, ("en_US", "UTF-8"))
